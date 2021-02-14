a1ddefe195ad4174c7309d3727c7b9970ac4b6c4
Anton Kharlamov <antonio100500@ya.ru>
2021-02-14 18:22:11 +0300

    hometask is ready

--- boilerplate/JS/script.js
+++ boilerplate/JS/script.js
@@ -1,2 +1,13 @@
 
-alert('You are at' +' ' + window.location);
+let totalCash = +prompt('Введите количество денег');
+let watchesCount = +prompt('Сколько часов куплено?');
+let earringsCount = +prompt('Сколько сережек в уши куплено?');
+
+let watchesSum = +prompt('Стоимость часов') * watchesCount;
+let earringsSum = +prompt('Стоимость серёжек') * earringsCount;
+if ( watchesSum + earringsSum <= totalCash) {
+    document.write('true');
+}
+    else {
+        document.write('false');
+    }
