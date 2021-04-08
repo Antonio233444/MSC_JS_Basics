b3174d34d8a68407e0b1b23b248e2c8205d796ae
Anton Kharlamov <antonio100500@ya.ru>
2021-04-08 23:22:16 +0300

    Hometask_05 правка дз

--- Hometask/hometask_05/JS/script.js
+++ Hometask/hometask_05/JS/script.js
@@ -1,26 +1,21 @@
 
-
+//Объявление переменных
 
 const APIkey = '40aa06b8277083f0f9e28e699379f669';
 
-/*const city = 'Kiev';*/
 
 const form = document.forms[0];
 
 
 
+const city = form.elements.first.value;
+
 
 const insert = document.querySelector('.total');
 
+
 const button = form.elements.submit;
 
-
-
-button.addEventListener('onsubmit', function() {
-
-  const city = form.elements.first.value;
-  
-})
 
 
 
@@ -29,16 +24,45 @@ const url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+A
 
 
 
-let xhr = new XMLHttpRequest(); //отправка запросов на сервер;
-
-xhr.open('GET',url, false); // настройка запросов на сервер;
-
-
-xhr.send();//запрос отправлен;
 
 
 
-form.onsubmit = function(e){
+
+
+
+//Написание функции
+
+
+button.addEventListener('onsubmit', function() {
+
+
+  form.onsubmit = function(e){
+    e.preventDefault();
+    if(xhr.status !=200) {
+      console.log(xhr.status +' '+ xhr.statusText);
+    }
+    else{
+      let DATA = JSON.parse(xhr.responseText);
+      insert.innerHTML = DATA.main.temp - 273;
+    }
+
+  
+  }
+
+  xhr.open('GET',url, false); // настройка запросов на сервер;
+
+
+  xhr.send();//запрос отправлен;
+  
+})
+
+
+
+
+
+
+
+/*form.onsubmit = function(e){
   e.preventDefault();
   if(xhr.status !=200) {
     console.log(xhr.status +' '+ xhr.statusText);
@@ -49,7 +73,7 @@ form.onsubmit = function(e){
   }
 
 }
-
+*/
 
 
 
